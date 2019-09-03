ceb834756d43c41f3bd534fade353c11ae59eb70
Max Turner <maxwellholteturner@gmail.com>
2019-09-03 14:04:55 -0700

    Added additional exclusions param for get hierarchy

--- visanalysis/plugin/base.py
+++ visanalysis/plugin/base.py
@@ -56,15 +56,17 @@ def getAttributesFromGroup(file_path, group_path):
         return attr_dict
 
 
-def getHierarchy(file_path):
+def getHierarchy(file_path, additional_exclusions=None):
     with h5py.File(file_path, 'r') as experiment_file:
-        hierarchy = recursively_load_dict_contents_from_group(experiment_file, '/')
+        hierarchy = recursively_load_dict_contents_from_group(experiment_file, '/', additional_exclusions=additional_exclusions)
     return hierarchy
 
 
-def recursively_load_dict_contents_from_group(h5file, path):
+def recursively_load_dict_contents_from_group(h5file, path, additional_exclusions=None):
     # https://codereview.stackexchange.com/questions/120802/recursively-save-python-dictionaries-to-hdf5-files-using-h5py
     exclusions = ['acquisition', 'Client', 'epochs', 'stimulus_timing', 'roipath', 'subpath']
+    if additional_exclusions is not None:
+        exclusions.append(additional_exclusions)
     ans = {}
     for key, item in h5file[path].items():
         if isinstance(item, h5py._hl.dataset.Dataset):
@@ -73,7 +75,7 @@ def recursively_load_dict_contents_from_group(h5file, path):
             if np.any([x in key for x in exclusions]):
                 pass
             else:
-                ans[key] = recursively_load_dict_contents_from_group(h5file, path + key + '/')
+                ans[key] = recursively_load_dict_contents_from_group(h5file, path + key + '/', additional_exclusions=additional_exclusions)
     return ans
 
 
