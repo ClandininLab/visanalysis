ec38fc7191462f4f0dd497b993fce885f825281d
Max <maxwellholteturner@gmail.com>
2020-06-23 08:39:24 -0700

    attach_data specify data dir

--- attach_data.py
+++ attach_data.py
@@ -5,7 +5,8 @@ import sys
 import os
 
 experiment_filepath = sys.argv[1]
-rigID = sys.argv[2]
+data_directory = sys.argv[2]
+rigID = sys.argv[3]
 
 
 if rigID == 'Bruker':
@@ -16,7 +17,6 @@ else:
     plug = plugin.base.BasePlugin()
 
 experiment_file_name = os.path.split(experiment_filepath)[-1].split('.')[0]
-data_directory = os.path.join(os.getcwd(), experiment_file_name.replace('-',''))
 
 plug.attachData(experiment_file_name, experiment_filepath, data_directory)
 
