091f3b6cb82efd9e6dd683cfe06d13c62b677e9f
Max Turner <maxwellholteturner@gmail.com>
2021-06-03 16:03:55 -0700

    query based filtering for shared analysis Faster more intuitive tools for yanking out trials based on parameter values. see filterTrials()

--- visanalysis/analysis/shared_analysis.py
+++ visanalysis/analysis/shared_analysis.py
@@ -11,82 +11,46 @@ import matplotlib.pyplot as plt
 from visanalysis import plot_tools
 
 
-def getTraceMatrixByStimulusParameter(response_matrix, parameter_values):
+def matchQuery(epoch_parameters, query):
     """
-    parameter values is nTrials x nParams  numpy array
-    returns:
-      uniqueParameterValues (nConditions x nParams)
-      meanTraceMatrix and semTraceMatrix (nRois x nConditions, time)
+
+    params:
+        epoch_parameters: single epoch_parameter dict
+        query: dict, key-value pairs indicate matching parameter editable_values
+            e.g. query = {'current_intensity': 0.75}
+
+    Returns:
+        Bool, True if all param values match, false otherwise
     """
-    unique_parameter_values = np.unique(parameter_values, axis=0)
+    return np.all([epoch_parameters.get(key) == query[key] for key in query])
 
-    no_rois = response_matrix.shape[0]
-    epoch_len = response_matrix.shape[2]
-    no_conditions = len(unique_parameter_values)
+def filterTrials(epoch_response, ID, query):
+    matching_trials = np.where([matchQuery(ep, query) for ep in ID.getEpochParameters()])[0]
 
-    mean_trace_matrix = np.empty(shape=(no_rois, no_conditions, epoch_len), dtype=float)
-    mean_trace_matrix[:] = np.nan
-
-    sem_trace_matrix = np.empty(shape=(no_rois, no_conditions, epoch_len), dtype=float)
-    sem_trace_matrix[:] = np.nan
-
-    individual_traces = []
-    for vInd, V in enumerate(unique_parameter_values):
-        pull_inds = np.where((parameter_values == V).all(axis=1))[0]
-        current_responses = response_matrix[:, pull_inds, :]
-        mean_trace_matrix[:, vInd, :] = np.mean(current_responses, axis=1)
-        sem_trace_matrix[:, vInd, :] = np.std(current_responses, axis=1) / np.sqrt(len(pull_inds))
-        individual_traces.append(current_responses)
-
-    return unique_parameter_values, mean_trace_matrix, sem_trace_matrix, individual_traces
+    return epoch_response[:, matching_trials, :]
 
 
 def plotResponseByCondition(ImagingData, roi_name, condition, eg_ind=0):
     roi_data = ImagingData.getRoiResponses(roi_name)
 
-    conditioned_param = []
-    for ep in ImagingData.getEpochParameters():
-        conditioned_param.append(ep[condition])
+    unique_parameter_values = np.unique([ep.get(condition) for ep in ImagingData.getEpochParameters()])
+    fh, ax = plt.subplots(1, len(unique_parameter_values), figsize=(8, 2))
+    [x.set_axis_off() for x in ax]
+    [x.set_ylim([-0.25, 1.0]) for x in ax]
+    for p_ind, param_value in enumerate(unique_parameter_values):
+        query = {condition: param_value}
+        trials = filterTrials(roi_data.get('epoch_response'), ImagingData, query)
+        ax[p_ind].plot(roi_data.get('time_vector'), np.mean(trials[eg_ind, :, :], axis=0), linestyle='-', color=ImagingData.colors[0])
 
-    parameter_values = np.array([conditioned_param]).T
-    unique_parameter_values, mean_trace_matrix, sem_trace_matrix, individual_traces = getTraceMatrixByStimulusParameter(roi_data.get('epoch_response'), parameter_values)
-
-    unique_params = np.unique(np.array(conditioned_param))
-
-    # plot stuff
-    fig_handle = plt.figure(figsize=(10,2))
-    grid = plt.GridSpec(1, len(unique_params), wspace=0.2, hspace=0.15)
-    fig_axes = fig_handle.get_axes()
-    ax_ind = 0
-
-    plot_y_max = np.max((1.1*np.max(mean_trace_matrix), 3))
-    plot_y_min = -1
-
-    for ind_v, val in enumerate(unique_params):
-        pull_ind = np.where((unique_parameter_values == val).all(axis = 1))[0][0]
-        ax_ind += 1
-        if len(fig_axes) > 1:
-            new_ax = fig_axes[ax_ind]
-            new_ax.clear()
-        else:
-            new_ax = fig_handle.add_subplot(grid[0,ind_v])
-
-        new_ax.plot(roi_data.get('time_vector'), mean_trace_matrix[eg_ind,pull_ind,:].T)
-        new_ax.set_ylim([plot_y_min, plot_y_max])
-        new_ax.set_axis_off()
-        new_ax.set_title(val)
-        if ind_v == 0:  # scale bar
-            plot_tools.addScaleBars(new_ax, 1, 1, F_value = -1, T_value = -0.2)
-    fig_handle.canvas.draw()
-
+        if p_ind == 0:  # scale bar
+            plot_tools.addScaleBars(ax[p_ind], dT=1, dF=0.5, F_value=-0.25, T_value = -0.2)
 
 def plotRoiResponses(ImagingData, roi_name):
-    plot_y_min = -0.5
-    plot_y_max = 2
-
     roi_data = ImagingData.getRoiResponses(roi_name)
 
     fh, ax = plt.subplots(1, int(roi_data.get('epoch_response').shape[0]+1), figsize=(6, 2))
+    [x.set_axis_off() for x in ax]
+    [x.set_ylim([-0.25, 1]) for x in ax]
 
     for r_ind in range(roi_data.get('epoch_response').shape[0]):
         time_vector = roi_data.get('time_vector')
@@ -100,14 +64,11 @@ def plotRoiResponses(ImagingData, roi_name):
                             current_mean - current_sem,
                             current_mean + current_sem,
                             alpha=0.5)
-        ax[r_ind].set_ylim([plot_y_min, plot_y_max])
-        ax[r_ind].set_axis_off()
         ax[r_ind].set_title(int(r_ind))
 
         if r_ind == 0: # scale bar
             plot_tools.addScaleBars(ax[r_ind], 1, 1, F_value = -0.1, T_value = -0.2)
 
-    fh.canvas.draw()
 
 def filterDataFiles(data_directory, target_fly_metadata={}, target_series_metadata={}):
     """
