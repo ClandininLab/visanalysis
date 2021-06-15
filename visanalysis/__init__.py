4caa9a900423428006778ac2482f0032700a167f
Max Turner <maxwellholteturner@gmail.com>
2021-06-15 12:44:54 -0700

    try no __inits__???

--- visanalysis/__init__.py
+++ visanalysis/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from . import (analysis, plugin, util)
+# from . import (analysis, plugin, util)
--- visanalysis/analysis/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# from . import (imaging_data, shared_analysis, volumetric_data)
