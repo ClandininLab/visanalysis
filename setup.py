729b27b43a9dc3910568a56f9cbb69cda40e8116
Max Turner <maxwellholteturner@gmail.com>
2021-06-15 13:30:06 -0700

    vers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='visanalysis',
-    version='0.1.0',
+    version='0.1.1',
     description='Analysis environment for visprotocol experiments',
     url='https://github.com/ClandininLab/visanalysis',
     author='Max Turner',
