abff19e49189978dbceddbf9fc6e4b42805aa5ec
Max Turner <maxwellholteturner@gmail.com>
2021-06-19 12:12:27 -0700

    mot_corr: save as nifti2 if necessary

--- scripts/motion_correction.py
+++ scripts/motion_correction.py
@@ -31,5 +31,9 @@ print('Loaded {}, shape={}'.format(file_base_path + '_channel_2.nii', ch2.shape)
 merged = registration.register_two_channels_to_red(ch1, ch2, spatial_dims=len(ch1.shape) - 1)
 
 # Save registered, merged .nii
-nib.save(nib.Nifti1Image(merged, np.eye(4)), file_base_path + '_reg.nii')
+nifti1_limit = (2**16 / 2)
+if np.any(np.array(merged.shape) >= nifti1_limit): # Need to save as nifti2
+    nib.save(nib.Nifti2Image(merged, np.eye(4)), file_base_path + '_reg.nii')
+else: # Nifti1 is OK
+    nib.save(nib.Nifti1Image(merged, np.eye(4)), file_base_path + '_reg.nii')
 print('Saved registered brain to {}. Total time = {:.1f}'.format(file_base_path + '_reg.nii', time.time()-t0))
