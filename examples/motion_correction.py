b487541675f6f4094309cf8e78d43122f73cfcb8
Max Turner <maxwellholteturner@gmail.com>
2022-02-09 16:17:14 -0800

    example scripts, guis in subdir. Analysis GUI doesn't use registration anymore

---
+++ examples/motion_correction.py
@@ -0,0 +1,39 @@
+"""
+Example motion correction script.
+
+https://github.com/ClandininLab/visanalysis
+mhturner@stanford.edu
+"""
+import sys
+import time
+import nibabel as nib
+import numpy as np
+from visanalysis.util import registration
+
+t0 = time.time()
+
+# first arg: path to image series base, without .suffix
+#   e.g. /oak/stanford/groups/trc/data/Max/ImagingData/Bruker/20210611/TSeries-20210611-001
+file_base_path = sys.argv[1]
+print('Registering brain file from {}'.format(file_base_path))
+
+# Load metadata from bruker .xml file
+metadata = registration.get_bruker_metadata(file_base_path + '.xml')
+print('Loaded metadata from {}'.format(file_base_path + '.xml'))
+
+# Load brain images
+ch1 = registration.get_ants_brain(file_base_path + '_channel_1.nii', metadata, channel=0)
+print('Loaded {}, shape={}'.format(file_base_path + '_channel_1.nii', ch1.shape))
+ch2 = registration.get_ants_brain(file_base_path + '_channel_2.nii', metadata, channel=0)
+print('Loaded {}, shape={}'.format(file_base_path + '_channel_2.nii', ch2.shape))
+
+# Register both channels to channel 1
+merged = registration.register_two_channels_to_red(ch1, ch2, spatial_dims=len(ch1.shape) - 1)
+
+# Save registered, merged .nii
+nifti1_limit = (2**16 / 2)
+if np.any(np.array(merged.shape) >= nifti1_limit): # Need to save as nifti2
+    nib.save(nib.Nifti2Image(merged, np.eye(4)), file_base_path + '_reg.nii')
+else: # Nifti1 is OK
+    nib.save(nib.Nifti1Image(merged, np.eye(4)), file_base_path + '_reg.nii')
+print('Saved registered brain to {}. Total time = {:.1f}'.format(file_base_path + '_reg.nii', time.time()-t0))
---
+++ examples/plotting_responses.py
@@ -0,0 +1,65 @@
+from visanalysis.analysis import imaging_data, shared_analysis
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+experiment_file_directory = '/Users/mhturner/CurrentData'
+experiment_file_name = '2021-05-20'
+series_number = 2
+
+file_path = os.path.join(experiment_file_directory, experiment_file_name + '.hdf5')
+
+# ImagingDataObject wants a path to an hdf5 file and a series number from that file
+ID = imaging_data.ImagingDataObject(file_path,
+                                    series_number,
+                                    quiet=True,
+                                    kwargs={'plot_trace_flag': False})
+
+# %% PARAMETERS & METADATA
+
+# run_parameters: dict
+run_parameters = ID.getRunParameters()
+
+
+# epoch_parameters: list of dicts, one for each epoch (trial)
+epoch_parameters = ID.getEpochParameters()
+
+# fly_metadata: dict
+fly_metadata = ID.getFlyMetadata()
+
+# acquisition_metadata: dict
+acquisition_metadata = ID.getAcquisitionMetadata()
+
+
+# %% ROIS AND RESPONSES
+
+# Get list of rois present in the hdf5 file for this series
+roi_set_names = ID.getRoiSetNames()
+
+# getRoiResponses() wants a ROI set name, returns roi_data (dict)
+roi_data = ID.getRoiResponses('LC12')
+roi_data.keys()
+
+# Plot whole ROI response across entire series
+fh0, ax0 = plt.subplots(1, 1, figsize=(12, 4))
+ax0.plot(roi_data.get('roi_response')[0].T)
+ax0.set_xlabel('Frame')
+ax0.set_ylabel('Avg ROI intensity')
+
+# Plot ROI response for trials 10 thru 15
+# 'epoch_response' is shape (rois, trials, time)
+fh1, ax1 = plt.subplots(1, 1, figsize=(6, 4))
+ax1.plot(roi_data.get('time_vector'), roi_data.get('epoch_response')[0, 10:15, :].T)
+ax1.set_ylabel('Response (dF/F)')
+ax1.set_xlabel('Time (s)')
+
+# %%
+
+ID.generateRoiMap(roi_name='LC12', z=3)
+
+# %%
+shared_analysis.plotRoiResponses(ID, roi_name='LC12')
+
+# %%
+shared_analysis.plotResponseByCondition(ID, roi_name='LC12', condition='current_diameter', eg_ind=0)
