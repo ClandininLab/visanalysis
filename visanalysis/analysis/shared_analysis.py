bfa6f3343fa49c80d41478d5a304423b1469fae9
Max Turner <maxwellholteturner@gmail.com>
2022-04-05 14:49:07 -0700

    matching series tools

--- visanalysis/analysis/shared_analysis.py
+++ visanalysis/analysis/shared_analysis.py
@@ -9,6 +9,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from visanalysis.util import plot_tools
+from collections import Sequence
 
 
 def matchQuery(epoch_parameters, query):
@@ -24,6 +25,7 @@ def matchQuery(epoch_parameters, query):
     """
     return np.all([epoch_parameters.get(key) == query[key] for key in query])
 
+
 def filterTrials(epoch_response, ID, query, return_inds=False):
     matching_trials = np.where([matchQuery(ep, query) for ep in ID.getEpochParameters()])[0]
 
@@ -32,9 +34,11 @@ def filterTrials(epoch_response, ID, query, return_inds=False):
     else:
         return epoch_response[:, matching_trials, :]
 
+
 def getUniqueParameterCombinations(param_keys, ID):
     ep_params = [[ep.get(x, None) for x in param_keys]for ep in ID.getEpochParameters()]
     return list({tuple(row) for row in ep_params})
+
 
 def plotResponseByCondition(ImagingData, roi_name, condition, eg_ind=0):
     roi_data = ImagingData.getRoiResponses(roi_name)
@@ -49,7 +53,8 @@ def plotResponseByCondition(ImagingData, roi_name, condition, eg_ind=0):
         ax[p_ind].plot(roi_data.get('time_vector'), np.mean(trials[eg_ind, :, :], axis=0), linestyle='-', color=ImagingData.colors[0])
 
         if p_ind == 0:  # scale bar
-            plot_tools.addScaleBars(ax[p_ind], dT=1, dF=0.5, F_value=-0.25, T_value = -0.2)
+            plot_tools.addScaleBars(ax[p_ind], dT=1, dF=0.5, F_value=-0.25, T_value=-0.2)
+
 
 def plotRoiResponses(ImagingData, roi_name):
     roi_data = ImagingData.getRoiResponses(roi_name)
@@ -61,19 +66,19 @@ def plotRoiResponses(ImagingData, roi_name):
     for r_ind in range(roi_data.get('epoch_response').shape[0]):
         time_vector = roi_data.get('time_vector')
         no_trials = roi_data.get('epoch_response')[r_ind, :, :].shape[0]
-        current_mean = np.mean(roi_data.get('epoch_response')[r_ind,:,:], axis=0)
-        current_std = np.std(roi_data.get('epoch_response')[r_ind,:,:], axis=0)
+        current_mean = np.mean(roi_data.get('epoch_response')[r_ind, :, :], axis=0)
+        current_std = np.std(roi_data.get('epoch_response')[r_ind, :, :], axis=0)
         current_sem = current_std / np.sqrt(no_trials)
 
         ax[r_ind].plot(time_vector, current_mean, 'k')
         ax[r_ind].fill_between(time_vector,
-                            current_mean - current_sem,
-                            current_mean + current_sem,
-                            alpha=0.5)
+                               current_mean - current_sem,
+                               current_mean + current_sem,
+                               alpha=0.5)
         ax[r_ind].set_title(int(r_ind))
 
-        if r_ind == 0: # scale bar
-            plot_tools.addScaleBars(ax[r_ind], 1, 1, F_value = -0.1, T_value = -0.2)
+        if r_ind == 0:  # scale bar
+            plot_tools.addScaleBars(ax[r_ind], 1, 1, F_value=-0.1, T_value=-0.2)
 
 
 def filterDataFiles(data_directory, target_fly_metadata={}, target_series_metadata={}, target_roi_series=[]):
@@ -116,14 +121,53 @@ def filterDataFiles(data_directory, target_fly_metadata={}, target_series_metada
 
                     all_series.append(new_series)
 
-
     # search in all series for target key/value pairs
     match_dict = {**target_fly_metadata, **target_series_metadata}
     matching_series = []
     for series in all_series:
-        if all(series[key] == match_dict[key] for key in match_dict):
+        if checkAgainstTargetDict(match_dict, series):
             if np.all([r in series.get('rois') for r in target_roi_series]):
                 matching_series.append(series)
 
+    matching_series = sorted(matching_series, key=lambda d: d['file_name'] + '-' + str(d['series']).zfill(3))
+
     print('Found {} matching series'.format(len(matching_series)))
     return matching_series
+
+
+def checkAgainstTargetDict(target_dict, test_dict):
+    for key in target_dict:
+        if key in test_dict:
+            if not areValsTheSame(target_dict[key], test_dict[key]):
+                return False  # Different values
+        else:
+            return False  # Target key not in this series at all
+
+    return True
+
+
+def areValsTheSame(target_val, test_val):
+
+    if isinstance(target_val, str):
+        return target_val.casefold() == test_val.casefold()
+    elif isinstance(target_val, bool):
+        return target_val == test_val
+    elif isinstance(target_val, (int, float)):  # Scalar
+        if isinstance(test_val, (int, float)):
+            return float(target_val) == float(test_val)  # Ignore type for int vs. float here
+        else:
+            return False
+    elif isinstance(target_val, (Sequence, np.ndarray)):  # Note already excluded possibility of string by if ordering
+        if isinstance(test_val, (Sequence, np.ndarray)):
+            # Ignore order of arrays, and ignore float vs. int
+            return np.all(np.sort(target_val, axis=0).astype(float) == np.sort(test_val, axis=0).astype(float))
+        else:
+            return False
+
+    else:
+        print('----')
+        print('Unable to match ')
+        print('Target {} ({})'.format(target_val, type(target_val)))
+        print('Test {} ({})'.format(test_val, type(test_val)))
+        print('----')
+        return False
