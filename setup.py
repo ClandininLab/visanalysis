4b6f3bc98ba2007bea8938eb081694e2a17ed314
Max Turner <maxwellholteturner@gmail.com>
2022-04-07 10:17:21 -0700

    remove ants dependence and registration stuff brainsss is better for moco, removed ants dependency and registration module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='visanalysis',
-    version='2.0.0',
+    version='2.0.1',
     description='Analysis environment for visprotocol experiments',
     url='https://github.com/ClandininLab/visanalysis',
     author='Max Turner',
