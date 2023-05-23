cbac45fe41a1c17962818fb0633ebc569a995abd
Minseung Choi <minseung@stanford.edu>
2023-05-22 20:00:26 -0700

    shared_analysis collections import

--- visanalysis/analysis/shared_analysis.py
+++ visanalysis/analysis/shared_analysis.py
@@ -9,7 +9,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from visanalysis.util import plot_tools
-from collections import Sequence
+from collections.abc import Sequence
 
 
 def matchQuery(epoch_parameters, query):
